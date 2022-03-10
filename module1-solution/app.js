ff0c1363d165f937a06d15ce8ed052a63092f15b
amrit96 <banerjee96.amritayan@gmail.com>
2022-03-10 11:46:51 +0530

    base solution

---
+++ module1-solution/app.js
@@ -0,0 +1,22 @@
+(function(){
+    'use strict'
+    angular.model('LunchCheck',[])
+    .controller('LunchCheckController',luch_counter);
+
+    luch_counter.$inject=['$scope']
+    function luch_counter($scope){
+        $scope.decide_quantity = function(){
+            lunch_items_count = $scope.lunchMenu.split(",").length;
+            if(lunch_items_count>3){
+                $scope.result = "Too much!"
+            }
+            else if(lunch_items_count==0){
+                $scope.result = "Please enter data first"
+            }
+            else{
+                $scope.result = "Enjoy!"
+            }
+        }
+        
+    }
+})();
